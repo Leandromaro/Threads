ee9270acb6b3027226b46def3959914105659883
Leandro Maro <leandro.maro@globant.com>
2019-01-16 19:06:36 -0300

    Adding producer and consumer example

--- ProducerAndConsumer/src/Consumer.java
+++ ProducerAndConsumer/src/Consumer.java
@@ -16,9 +16,13 @@ public class Consumer implements Runnable {
         try {
             while (true){
                 producedSemaphore.acquire(); // Checks if it can access to the Producer
+                // Before consumer can consume an item,
+                // it must acquire a permit
                 mutex.acquire(); // it's checking if we can access to the Queue resource
                 String product = produced.poll();
                 System.out.println(Thread.currentThread().getName() +" got: "+ product);
+                // After consumer consumes the item,
+                // it releases consumer to notify producer
                 mutex.release();
             }
         }catch (InterruptedException e){
